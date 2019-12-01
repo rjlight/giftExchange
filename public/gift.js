0112eb11bf12c9d18190a0662f454707385f2b1e
Rachel Jones <rlbluegirl101@gmail.com>
2019-11-30 18:15:33 -0700

    commented out server stuff due to db not in use yet

--- public/gift.js
+++ public/gift.js
@@ -1,27 +1,27 @@
 function logIn() {
-    var request = new XMLHttpRequest()
+    //var request = new XMLHttpRequest()
 
     // Open a new connection, using the POST request on the URL endpoint
-    request.open('POST', '', true) //for the database when up and running
+    //request.open('POST', '', true) //for the database when up and running
   
-    request.onload = function() {
+   // request.onload = function() {
       // Begin accessing JSON data here
-      var data = JSON.parse(this.response)
+    //  var data = JSON.parse(this.response)
   
       //will be accessing names from the db
-      if (request.status >= 200 && request.status < 400) {
+     /* if (request.status >= 200 && request.status < 400) {
         for (i in data) { //put the names in a list
                   var txt = "<li>" + data[i] + "</li><br>" ;
             } 
-      } else if (1 == 1) {
+      } else*/ if (1 == 1) {
         var txt = "<li>" + "AJAX request worked!" + "</li><br>" ;
       } else  {
         console.log('error')
       }
           document.getElementById("group").innerHTML = txt;
-    }
+    //}
       // Send request
-      request.send()
+     // request.send()
 }
 function newUser() {
     //query database to create a new group
@@ -33,30 +33,30 @@ function addSingle() {
     //query db to add single person
 }
 function assignNames() {
-    var request = new XMLHttpRequest()
+    //var request = new XMLHttpRequest()
 
     // Open a new connection, using the POST request on the URL endpoint
-    request.open('POST', '', true) //for the database when up and running
+   // request.open('POST', '', true) //for the database when up and running
     
-    request.onload = function() {
+   // request.onload = function() {
         // Begin accessing JSON data here
-        var data = JSON.parse(this.response)
+     //   var data = JSON.parse(this.response)
     
         //will be accessing names from the db
-        if (request.status >= 200 && request.status < 400) {
+    /*    if (request.status >= 200 && request.status < 400) {
         for (i in data) { //put the names in a list
                     var txt = "<li>" + data[i] + "</li><br>" ;
             } 
-        } else if (1 == 1) {
+        } else */if (1 == 1) {
         var txt = "<li>" + "AJAX request worked again! - Assigned names!" + "</li><br>";
         txt = "<li>" + "Me - You" + "</li><br>"
         } else  {
         console.log('error')
         }
             document.getElementById("group").innerHTML = txt;
-    }
+   // }
         // Send request
-        request.send()
+      //  request.send()
 }
 function emailGroup() {
     //do something to send this email list to the whole group 
